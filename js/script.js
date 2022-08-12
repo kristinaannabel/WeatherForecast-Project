2e852ba92b49164dc29f6b0b7da1ba6335284d87
kristinaannabel <100444032+kristinaannabel@users.noreply.github.com>
2022-08-12 17:24:56 +0200

    Added real data to the forecast

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* I am blocked from the API for 2,5 days so I couldnt implement the 
+icons on the forecast. But the rest was working because I was blocked*/
+
+
 // Current Time
 const now = new Date();
 let hour = now.getHours();
@@ -51,49 +55,47 @@ const cityInput = document.querySelector("input");
 //Api Keys
 const apiKey = "96e1b7384a3ea207c6c443d025e59895";
 
-
 function getInfo(response) {
-    temp = Math.round(response.data.main.temp);
-    wind = Math.round(response.data.wind.speed);
-    humidity = response.data.main.humidity;
-    clouds = response.data.weather[0].description;
-    
-    cityElement.innerHTML = response.data.name;
-    tempElement.innerHTML = `${temp} C° `;
-    humidityElement.innerHTML = `${humidity}%`;
-    windElement.innerHTML = `${wind} km/h`;
-    cloudsElement.innerHTML = `${clouds}`;
-    cloudIconElement.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-      
-      getSearchForecast(response.data.coord);
-    }
-    
-    function searchCity(event) {
-        event.preventDefault();
-        cityElement.innerHTML = cityInput.value;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
-        axios
-        .get(apiUrl)
-        .then((response) => {
-            getInfo(response);
-        })
-        .catch((e) => {
-        cityElement.innerHTML = e.response.data.message;
-        });
-    }
-    
-    // Current location
-    function getUserLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
+  temp = Math.round(response.data.main.temp);
+  wind = Math.round(response.data.wind.speed);
+  humidity = response.data.main.humidity;
+  clouds = response.data.weather[0].description;
+
+  cityElement.innerHTML = response.data.name;
+  tempElement.innerHTML = `${temp} C° `;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind} km/h`;
+  cloudsElement.innerHTML = `${clouds}`;
+  cloudIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  getSearchForecast(response.data.coord);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  cityElement.innerHTML = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      getInfo(response);
+    })
+    .catch((e) => {
+      cityElement.innerHTML = e.response.data.message;
+    });
+}
+
+// Current location
+function getUserLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
     axios.get(apiUrl).then(getInfo);
-});
+  });
 }
-
 
 // Toggle celsius-farenheit
 function toggleMeasure() {
@@ -110,33 +112,36 @@ submitInput.addEventListener("submit", searchCity);
 
 // Forecast
 function getSearchForecast(coordinates) {
-    console.log(coordinates);
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayForecast);
+  console.log(coordinates);
+  let apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${coordinates.lat}&longitude=${coordinates.lon}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,windspeed_10m_max&timezone=Europe%2FBerlin`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-console.log(response.data);
-  let forecastElement = document.getElementById("forecast");
-  let forecastHtml = `<div class="row">`
-  days.forEach( function (day) {
-      forecastHtml = forecastHtml + `
+  let forecast = response.data.daily;
+  console.log(forecast);
+  days.forEach((day, index) => {
+    const myDay = document.createElement("row");
+    myDay.innerHTML = `
+    <div class="row">
       <div class="weekday">
-      <b>${day}</b>
+        <b>${day}</b>
       </div>
       <div class="weather-icon">
-      <img 
-         src="https://th.bing.com/th/id/R.bb277892fc034fac5191968584aa8ead?rik=hbASBK6hDwFGNw&riu=http%3a%2f%2fclipartmag.com%2fimages%2fcloud-icon-png-22.png&ehk=ozP7LioKT%2bAGB2RU2A%2ffxoGRjbHsiryW3oxDcrG1HF8%3d&risl=&pid=ImgRaw&r=0"
-         alt="weather description icon"
-         width="30"
-         />   
-         </div>
-         <div class="temp">
-         <i class="fa-solid fa-temperature-full"></i> 20°C
-         </div> 
-         </br>
-         </div>
-         `;
-        });
-  forecastElement.innerHTML = forecastHtml;
-}  
+        <img 
+          src="https://cdn4.iconfinder.com/data/icons/biticon-weather-line/24/weather_sun_sunny_day-512.png"
+          alt="weather description icon"
+          width="30"
+        />   
+      </div>
+      <div class="temp">
+        <i class="fa-solid fa-temperature-full"></i> ${Math.round(
+          forecast.temperature_2m_max[index]
+        )}°C
+      </div> 
+      </br>
+    </div>
+    `;
+    document.querySelector("#forecast").appendChild(myDay);
+  });
+}
